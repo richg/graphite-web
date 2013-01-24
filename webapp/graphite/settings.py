94f47900debf13a96003af8ac2dcf3a98c1f6972
Richard Graham <richardg@sjsoft.com>
2013-01-24 14:29:42 +0200

    Change the version from 0.9.10 to 0.9.10-warden to avoid confusion with the actual 0.9.10 version.

--- webapp/graphite/settings.py
+++ webapp/graphite/settings.py
@@ -23,7 +23,7 @@ except ImportError:
   rrdtool = False
 
 GRAPHITE_WEB_APP_SETTINGS_LOADED = False
-WEBAPP_VERSION = '0.9.10'
+WEBAPP_VERSION = '0.9.10-warden'
 DEBUG = False
 JAVASCRIPT_DEBUG = False
 
